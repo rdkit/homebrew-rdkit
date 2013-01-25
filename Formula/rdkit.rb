a4d6554b8a76bc55bc88bc75a6d0ac0ae58b83e0
Eddie Cao <cao.yiqun@gmail.com>
2013-01-24 21:17:31 -0800

    update to 2012Q4 release

--- Formula/rdkit.rb
+++ Formula/rdkit.rb
@@ -10,9 +10,9 @@ end
 
 class Rdkit < Formula
   homepage 'http://rdkit.org/'
-  url 'http://sourceforge.net/projects/rdkit/files/rdkit/Q3_2012/RDKit_2012_09_1.tgz'
+  url 'http://sourceforge.net/projects/rdkit/files/rdkit/Q4_2012/RDKit_2012_12_1.tgz'
   head 'http://svn.code.sf.net/p/rdkit/code/trunk/', :using=> :svn
-  sha1 'cae543325dd40d8f8ed09dd58fa4504dde153382'
+  sha1 '42c40929ca43ac29bf4795e08832045a00e28c3a'
 
   depends_on 'cmake' => :build
   depends_on 'wget' => :build
@@ -77,10 +77,6 @@ class Rdkit < Formula
     rm_f Dir["#{lib}/*.cmake"]
   end
 
-  def patches
-    DATA
-  end
-
   def caveats
     python_lib = `python --version 2>&1| sed -e 's/Python \\([0-9]\\.[0-9]\\)\\.[0-9]/python\\1/g'`.strip
 
@@ -96,37 +92,3 @@ class Rdkit < Formula
     EOS
   end
 end
-
-__END__
---- a/CMakeLists.txt  2012-10-19 23:47:44.000000000 -0700
-+++ b/CMakeLists.txt	2012-10-21 23:35:13.000000000 -0700
-@@ -151,6 +151,7 @@
-   set(Boost_THREAD_LIBRARY )
- endif()
-
-+find_package(Boost 1.39.0 COMPONENTS system REQUIRED)
-
- # setup our compiler flags:
- if(CMAKE_COMPILER_IS_GNUCXX)
---- a/Code/GraphMol/CMakeLists.txt	2012-06-29 22:27:14.000000000 -0700
-+++ b/Code/GraphMol/CMakeLists.txt	2012-10-21 23:36:25.000000000 -0700
-@@ -5,7 +5,7 @@
-               AtomIterators.cpp BondIterators.cpp Aromaticity.cpp Kekulize.cpp
-               MolDiscriminators.cpp ConjugHybrid.cpp AddHs.cpp RankAtoms.cpp
-               Matrices.cpp Chirality.cpp RingInfo.cpp Conformer.cpp
--              SHARED LINK_LIBRARIES RDGeometryLib RDGeneral)
-+	      SHARED LINK_LIBRARIES RDGeometryLib RDGeneral ${Boost_SYSTEM_LIBRARY})
-
- rdkit_headers(Atom.h
-               atomic_data.h
---- a/Code/JavaWrappers/gmwrapper/CMakeLists.txt  2012-10-24 22:22:15.000000000 -0700
-+++ b/Code/JavaWrappers/gmwrapper/CMakeLists.txt	2012-10-01 22:57:23.000000000 -0700
-@@ -74,7 +74,7 @@
-
- SWIG_ADD_MODULE(GraphMolWrap "java" GraphMolJava.i )
-
--SWIG_LINK_LIBRARIES(GraphMolWrap ${RDKit_Wrapper_Libs} )
-+SWIG_LINK_LIBRARIES(GraphMolWrap ${RDKit_Wrapper_Libs} ${Boost_SYSTEM_LIBRARY})
-
- # code adapted from the wrapper code for
- # GDCM: http://gdcm.svn.sf.net/viewvc/gdcm/trunk/Wrapping/Java/CMakeLists.txt?view=markup
