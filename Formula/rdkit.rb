22f1ab4d2f626a4406fd08b43ca271f176d73b3c
Brian Kelley <brian.kelley@novartis.com>
2018-04-07 10:43:14 -0400

    remove deprecated undent

--- Formula/rdkit.rb
+++ Formula/rdkit.rb
@@ -85,13 +85,13 @@ class Rdkit < Formula
   end
 
   def caveats
-    s = <<-EOS.undent
+    s = <<-EOS
       You may need to add RDBASE to your environment variables.
       For Bash, put something like this in your $HOME/.bashrc:
         export RDBASE=#{HOMEBREW_PREFIX}/share/RDKit
     EOS
     if build.with? "java"
-      s += <<-EOS.undent
+      s += <<-EOS
 
         The RDKit Jar file has been installed to:
           #{libexec}/org.RDKit.jar
